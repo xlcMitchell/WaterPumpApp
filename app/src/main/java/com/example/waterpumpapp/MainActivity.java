d7154a1a7f6664d90deff2e62717dd8cbdce1aef
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-12-07 12:41:18 +1300

    main activity xml fie created

--- app/src/main/java/com/example/waterpumpapp/MainActivity.java
+++ app/src/main/java/com/example/waterpumpapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.waterpumpapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button waterPlant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        waterPlant = findViewById(R.id.btnWater);
+
+        waterPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
